53a56f519a4bbecac8fb1bfacc4c262c30b29232
TurchaninovStudent <aeturchaninov@ya.ru>
2026-01-17 14:20:43 +0300

    pre-review

--- app/src/main/java/com/example/firstandroidapplication/firstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/firstandroidapplication/firstandroidapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.firstandroidapplication.firstandroidapp
 
+import kotlin.random.Random
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textToInputIn: TextView = findViewById(R.id.tutorialText)
+        val button: Button = findViewById(R.id.roll)
+
+        button.setOnClickListener {
+            button.rotationX = 0.0f
+            button.animate().rotationX(360.0f).setDuration(400).start()
+            setRandom(textToInputIn)
+        }
+    }
+
+    private fun setRandom(textToInputIn : TextView) {
+        textToInputIn.text = Random.nextInt(1, 6).toString()
+        textToInputIn.textSize = 48.0f;
     }
 }
