9428dab67d5f41d32ac5ff5f11c91523d313be7c
689-23 <aeturchaninov@ya.ru>
2026-01-24 13:36:09 +0300

    after second task, made binding pre-review

--- app/src/main/java/com/example/firstandroidapplication/firstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/firstandroidapplication/firstandroidapp/MainActivity.kt
@@ -2,36 +2,41 @@ package com.example.firstandroidapplication.firstandroidapp
 
 import kotlin.random.Random
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.firstandroidapplication.firstandroidapp.databinding.ActivityMainBinding
 
+/**
+ * Главная View приложения
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val textToInputIn: TextView = findViewById(R.id.tutorialText)
-        val button: Button = findViewById(R.id.roll)
+        val button = binding.roll
 
         button.setOnClickListener {
             button.rotationX = 0.0f
             button.animate().rotationX(360.0f).setDuration(400).start()
-            setRandom(textToInputIn)
+            setRandom()
         }
     }
 
-    private fun setRandom(textToInputIn : TextView) {
-        textToInputIn.text = Random.nextInt(1, 6).toString()
+    private fun setRandom() {
+        val textToInputIn = binding.tutorialText
+        textToInputIn.text = Random.nextInt(1, 7).toString()
         textToInputIn.textSize = 48.0f
     }
 }
